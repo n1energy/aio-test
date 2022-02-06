3467b5b760fcf28bf3f70ae20678100c96ee1138
well <you@example.com>
2022-02-06 18:49:04 +1000

    comleted all

--- app/crm/views.py
+++ app/crm/views.py
@@ -1,28 +1,51 @@
 import email
 import uuid
-from aiohttp.web_exceptions import HTTPNotFound
-from aiohttp.web_response import json_response 
+from aiohttp.web_exceptions import HTTPNotFound, HTTPUnauthorized,HTTPForbidden
+from aiohttp_apispec import docs, querystring_schema, request_schema, response_schema
 from app.crm.models import User
+from app.crm.schemes import ListUsersResponseSchema, UserGetRequestSchema, UserSchema, UserAddSchema
 from app.web.app import View
+from app.web.schemes import OkResponseSchema
+from app.web.utils import check_basic_auth, json_response
+
 
 class AddUserView(View):
+    @docs(tags=['crm'], summary='Add new user', description='Add new user to db')
+    @request_schema(UserAddSchema)
+    @response_schema(OkResponseSchema, 200)
     async def post(self):
-        data = await self.request.json()
+        data = self.request['data'] #cheto tut ne tak s async
         user = User(email=data["email"], id_=uuid.uuid4())
         await self.request.app.crm_accessor.add_user(user)
-        return json_response(data={"status": "ok"})
+        return json_response()
+
 
 class ListUsersView(View):
+    @docs(tags=['crm'], summary='List users', description='List users from db')
+    @response_schema(ListUsersResponseSchema, 200)
     async def get(self):
+        if not self.request.headers.get('Authorization'):
+            raise HTTPUnauthorized
+        if not check_basic_auth(self.request.headers['Authorization'], username = self.request.app.config.username, password=self.request.app.config.password):
+            raise HTTPForbidden
         users = await self.request.app.crm_accessor.list_users()
-        raw_users = [{'email': user.email, 'id': str(user.id_)} for user in users]
-        return json_response(data={'status': 'ok', 'users': raw_users})
+        raw_users = [UserSchema().dump(user) for user in users]
+        return json_response(data={'users': raw_users})
+
 
 class GetUserView(View):
+    @docs(tags=['crm'], summary='Get user', description='Get user from db')
+    @querystring_schema(UserGetRequestSchema)
+    @response_schema(ListUsersResponseSchema, 200)
     async def get(self):
+        if not self.request.headers.get('Authorization'):
+            raise HTTPUnauthorized
+        if not check_basic_auth(self.request.headers['Authorization'], username=self.request.app.config.username,
+                                password=self.request.app.config.password):
+            raise HTTPForbidden
         user_id = self.request.query['id']
         user = await self.request.app.crm_accessor.get_user(uuid.UUID(user_id))
         if user:
-            return json_response(data={'status': 'ok', 'user': {'email': user.email, 'id': str(user.id_)}})
+            return json_response(data={'user': UserSchema().dump(user)})
         else:
             raise HTTPNotFound
--- app/web/app.py
+++ app/web/app.py
@@ -1,12 +1,19 @@
 from typing import Optional
-from aiohttp.web import Application as AiohttpApplication, run_app as aiohttp_run_app, View as AiohttpView, Request as AiohttpRequest
+from aiohttp.web import Application as AiohttpApplication, run_app as aiohttp_run_app, View as AiohttpView, \
+    Request as AiohttpRequest
+from aiohttp_apispec import setup_aiohttp_apispec
 from app.store import setup_accessors
 from app.store.crm.accessor import CrmAccessor
+from app.web.config import Config, setup_config
+from app.web.middlewares import setup_middlewares
 from app.web.routes import setup_routes
 
+
 class Application(AiohttpApplication):
-    database : dict = {}
+    config: Optional[Config] = None
+    database: dict = {}
     crm_accessor: Optional[CrmAccessor] = None
+
 
 class Request(AiohttpRequest):
     @property
@@ -19,9 +26,14 @@ class View(AiohttpView):
     def request(self) -> Request:
         return super().request
 
+
 app = Application()
 
+
 def run_app():
+    setup_config(app)
     setup_routes(app)
+    setup_aiohttp_apispec(app, title='CRM App', url='/docs/json', swagger_path='/docs')
+    setup_middlewares(app)
     setup_accessors(app)
     aiohttp_run_app(app)
